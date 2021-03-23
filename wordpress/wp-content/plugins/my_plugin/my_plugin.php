48e8d42e5b452f770b3b7306bbbf806e30a73ebd
DIDON Maya <maya.didon@etu.imt-lille-douai.fr>
2021-03-23 12:05:12 +0100

    wordpress

---
+++ wordpress/wp-content/plugins/my_plugin/my_plugin.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Plugin Name: My plugin
+Plugin URI: http://localhost/wordpress/
+Description: Ceci est mon premier plugin
+Author: Maya
+Version: 0.1
+Author URI: http://localhost/wordpress/
+*/
+
