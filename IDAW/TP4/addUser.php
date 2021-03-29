8de408e1c590c0dc73c9c7318c9cb90103cf5d6f
DIDON Maya <maya.didon@etu.imt-lille-douai.fr>
2021-03-29 11:48:01 +0200

    modif crud ajax

--- IDAW/TP4/addUser.php
+++ IDAW/TP4/addUser.php
@@ -1,14 +1,35 @@
 <?php
-    $conn= mysqli_connect("localhost", "root", "", "testIDAW");
-    $sql="SELECT * FROM utilisateurstp4";
-    $result=$conn->query($sql);
-
-    $nom="anonymous";
-    $prenom="anonymous";
-    $datenaiss="";
-    $aimecours="false";
-    $remarques="";
-    $errorText="";
+    $conn= mysqli_connect("localhost", "root", "", "testidaw");
+    
+    if($conn == false){
+        die("ERROR: Could not connect. " . mysqli_connect_error());
+    }
+    if(isset($_POST['nom'] )){
+        $nom = $_POST['nom'];
+    }
+    if(isset($_POST['prenom'] )){
+        $prenom = $_POST['prenom'];
+    }
+    if(isset($_POST['datenaiss'] )){
+        $datenaiss = $_POST['datenaiss'];
+    }
+    if(isset($_POST['remarque'] )){
+        $remarque = $_POST['remarque'];
+    }
+    if($_POST['crud']=='ajout'){
+        $sql = "INSERT INTO utilisateurstp4 (nom,prenom,date_de_naissance,remarques) VALUES ('${nom}','${prenom}','${datenaiss}','${remarque}')";
+    }
+    if($_POST['crud']=='modif'){
+        $sql = "UPDATE utilisateurstp4 SET nom='${nom}',prenom='${prenom}',date_de_naissance='${datenaiss}',remarques='${remarque}' WHERE nom='${nom}'";
+    }
+    if($_POST['crud']=='suppr'){
+        $sql = "DELETE FROM utilisateurstp4 WHERE nom='${nom}'";
+    }
+    if(mysqli_query($conn, $sql)){
+        echo "Records added successfully.";
+    } else{
+        echo "ERROR: Could not able to execute $sql. " . mysqli_error($conn);
+    }
 
     
 ?>
